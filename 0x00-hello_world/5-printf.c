c25c00c73f44d819d6d49af360dc350fe4865582
Brian <brian.adul@yahoo.com>
2022-09-12 15:29:48 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,4 @@
 #include <stdio.h>
-/**
- *  *  main - this is the entry point for the code
- *   *
- *    *  Return: 0 if exited properly, non-zero otherwise
- *     */
-
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
