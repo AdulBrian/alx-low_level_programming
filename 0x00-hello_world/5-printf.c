2f72b202e7639a7911512445c215c0a7638d36b9
Brian Owuor Adul <111014873+AdulBrian@users.noreply.github.com>
2022-09-13 03:08:23 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-//main - the entry point for the code
-//prints proper grammar, but the outcome is a piece of art
-
+/*main - the entry point for the code*/
+/*prints proper grammar, but the outcome is a piece of art*/
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
