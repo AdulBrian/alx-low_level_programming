8ac88501bf36e26ed328a71a5a33d4e9d8bce9e1
Brian <brian.adul@yahoo.com>
2022-09-14 11:16:12 -0700

    print single numbers less than 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers les than 10,
+ *   * a new line follows
+ *    * Return: Aways 0 (if Sucessful)
+ *     */
+
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+printf("%d", n);
+printf("\n");
+return (0);
+}
