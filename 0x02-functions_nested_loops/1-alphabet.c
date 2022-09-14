5cf9872be5be27106675867bcc5b4f9a5b1023d2
Brian <brian.adul@yahoo.com>
2022-09-14 15:59:06 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include"main.h"
+/*main- print alphabets*/
+/*next line*/
+
+int main()
+{
+for(int x = 'a'; x <= 'z'; x++) // Notice (x <= 'Z') > > (x <= 'z')
+{
+
+putchar(x);
+}
+return 0;
+}
