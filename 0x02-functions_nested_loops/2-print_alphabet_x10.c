e714a9ae03e302c49f246ceb9d752bf985002270
Brian <brian.adul@yahoo.com>
2022-09-15 11:24:10 -0700

    print alphabets 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,15 @@ void  print_alphabet_x10(void)
 {
 char alphabet;
 int num = 1;
-	while (num <= 10)
-	{
-	alphabet = 'a';
-	while (alphabet <= 'z')
-	{
-	_putchar(alphabet);
-       	alphabet++;
-	}
-	_putchar('\n');
-	num++;
-	}
+while (num <= 10)
+{
+alphabet = 'a';
+while (alphabet <= 'z')
+{
+_putchar(alphabet);
+  alphabet++;
+}
+_putchar('\n');
+num++;
+}
 }
