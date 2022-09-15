01f6cea1dd950aa70057469d84a9b2af61f22180
Brian <brian.adul@yahoo.com>
2022-09-15 15:46:32 -0700

    update print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,10 +8,12 @@ void print_triangle(int size)
 int i;
 int j;
 int k;
+
 if (size <= 0)
 {
 _putchar('\n');
-} else
+}
+else
 {
 for (i = 1; i <= size; i++)
 {
@@ -26,4 +28,5 @@ _putchar('#');
 _putchar('\n');
 }
 }
+
 }
